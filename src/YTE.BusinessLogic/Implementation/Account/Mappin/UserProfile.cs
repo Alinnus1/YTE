952964d5ff0778718d5832d786fd59091e18667e
Alinnus1 <alinnus1@gmail.com>
2022-07-02 00:21:28 +0300

    Small bug fix

--- src/YTE.BusinessLogic/Implementation/Account/Mappin/UserProfile.cs
+++ src/YTE.BusinessLogic/Implementation/Account/Mappin/UserProfile.cs
@@ -11,12 +11,18 @@ namespace YTE.BusinessLogic.Implementation.Account.Mappin
         {
             CreateMap<RegisterModel, User>()
                 .ForMember(a => a.Id, a => a.MapFrom(s => Guid.NewGuid()))
+                .ForMember(a=>a.Name, a=> a.MapFrom(s=> s.LastName))
+                .ForMember(a=>a.Pronoun, a=> a.MapFrom(s=> s.FirstName))
                 .ForMember(a => a.PasswordHash, a => a.MapFrom(s => s.Password));
 
             CreateMap<User, EditUserAccountModel>()
-                .ForMember(a => a.Image, opt => opt.Ignore());
+                .ForMember(a => a.Image, opt => opt.Ignore())
+                .ForMember(a => a.LastName, a=> a.MapFrom(s=> s.Name))
+                .ForMember(a => a.FirstName, a=> a.MapFrom(s=> s.Pronoun));
 
             CreateMap<EditUserAccountModel, User>()
+                .ForMember(a => a.Name, a => a.MapFrom(s => s.LastName))
+                .ForMember(a => a.Pronoun, a => a.MapFrom(s => s.FirstName))
                 .ForMember(a => a.Image, opt => opt.Ignore());
 
             CreateMap<User, DetailsUserAccountModel>()
